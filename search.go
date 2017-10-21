999bec3155b3b99bb34f5e60ffa92f113de24a3d
Jas <7893@users.noreply.github.com>
2017-10-21 10:46:28 +0800

    search file function in go
    一个用 Go 实现的搜索功能（开个玩笑，只是添加描述而已）。

---
+++ search.go
@@ -0,0 +1,43 @@
+package search
+
+import (
+	"log"
+	"sync"
+)
+
+var matchers = make(map[string]Matcher)
+
+func Run(searchTerm string) {
+	feeds, err := RetrieveFeeds()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	results := make(chan *Result)
+
+	var watiGroup sync.WaitGroup
+
+	watiGroup.Add(len(feeds))
+
+	for _, feed := range feeds {
+		matcher, exists := matchers[feed.Type]
+
+		if !exists {
+			matcher = matchers["default"]
+		}
+
+		go func(matcher Matcher, feed *Feed) {
+			Match(matcher, feed, searchTerm, results)
+			watiGroup.Done()
+
+		}(matcher, feed)
+	}
+
+	go func() {
+		watiGroup.Wait()
+		close(results)
+	}()
+
+	Display(results)
+}
